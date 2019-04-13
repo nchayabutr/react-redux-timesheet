3133d6dbe8ea71d4e24f5f26ac539e6a98f2bb90
Noppadol Chayabutr <nchayabutr@gmail.com>
2019-04-13 14:47:39 -0500

    We are validating forms

--- lab-05/src/App.js
+++ lab-05/src/App.js
@@ -1,37 +1,38 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-import { Redirect, Route, Switch } from 'react-router';
-import Projects from "./projects/Projects";
-import Employees from './employees/Employees';
-import Timesheets from './timesheets/Timesheets';
-import Navigation from './nav/Navigation';
-import ProjectsDetail from './projects/ProjectsDetail';
-import TimesheetsDetail from './timesheets/TimesheetsDetail';
-import EmployeeDetail from './employees/EmployeeDetail';
-import TimeunitsDetail from './timeunits/TimeunitsDetail';
+import React from 'react'
+import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router'
+import Projects from './projects/Projects'
+import Employees from './employees/Employees'
+import Timesheets from './timesheets/Timesheets'
+import Navigation from './nav/Navigation'
+import ProjectsDetail from './projects/ProjectsDetail'
+import TimesheetsDetail from './timesheets/TimesheetsDetail'
+import EmployeeDetail from './employees/EmployeeDetail'
+import TimeunitsDetail from './timeunits/TimeunitsDetail'
 
 class App extends React.Component {
-  render() {
+  render () {
     return (
       <BrowserRouter>
-        <div className="App">
+        <div className='App'>
           <Navigation />
-          <div className="container">
+          <div className='container'>
             <Switch>
-              <Route exact path="/projects" component={Projects} />
-
-              <Route exact path="/employees" component={Employees} />
-
-              <Route exact path="/timesheets" component={Timesheets} />
-
-              <Redirect to="/employees" />
+              <Route exact path='/projects' component={Projects} />
+              <Route exact path='/projects/detail/:_id?' component={ProjectsDetail} />
+              <Route exact path='/employees' component={Employees} />
+              <Route exact path='/employees/detail/:_id?' component={EmployeeDetail} />
+              <Route exact path='/timesheets' component={Timesheets} />
+              <Route exact path='/timesheets/detail/:_id?' component={TimesheetsDetail} />
+              <Route exact path='/timesheets/detail/:timesheet_id/timeunits/detail/:_id?' component={TimeunitsDetail} />
+              <Redirect to='/employees' />
             </Switch>
           </div>
         </div>
       </BrowserRouter>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- lab-05/src/employees/Employees.js
+++ lab-05/src/employees/Employees.js
@@ -1,43 +1,48 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import EmployeeTable from './EmployeeTable';
-import * as EmployeeActionCreators from '../actions/EmployeeActionCreator';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import React from 'react'
+import PropTypes from 'prop-types'
+import EmployeeTable from './EmployeeTable'
+import * as EmployeeActionCreators from '../actions/EmployeeActionCreator'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 
 class Employees extends React.Component {
-  componentDidMount() {
-    const { listEmployees } = this.props;
-    listEmployees();
+  componentDidMount () {
+    const { listEmployees } = this.props
+    listEmployees()
   }
 
-  render() {
-    const { employees, deleteEmployee, restoreEmployee } = this.props;
+  render () {
+    const { employees, deleteEmployee, restoreEmployee } = this.props
 
     return (
       <div>
         <h1>Employees</h1>
-        <EmployeeTable employees={ employees } onDelete={ deleteEmployee } onRestore={ restoreEmployee }/>
+        <EmployeeTable employees={employees} onDelete={deleteEmployee} onRestore={restoreEmployee} />
+        <Link to='/employees/detail'>
+        <Button bsStyle='primary'>
+          New Employee
+        </Button>
+        </Link>
       </div>
-    );
+    )
   }
 }
 
 Employees.propTypes = {
   employees: PropTypes.arrayOf(PropTypes.object)
-};
+}
 
 const mapStateToProps = state => {
   return {
-    employees: state.employees.data,
-  };
-};
+    employees: state.employees.data
+  }
+}
 
 const mapDispatchToProps = {
   listEmployees: EmployeeActionCreators.listEmployees,
   deleteEmployee: EmployeeActionCreators.removeEmployee,
   restoreEmployee: EmployeeActionCreators.restoreEmployee
-};
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Employees);
+export default connect(mapStateToProps, mapDispatchToProps)(Employees)
